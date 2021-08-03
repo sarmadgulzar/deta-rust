2c546ba8cd9facf5c41ce29a5c7c90a7ed8f1336
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2021-08-03 05:20:39 +0500

    Added Base struct and get method

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
-# rust-deta
+# data-rust
 
 Deta SDK for Rust
+
+Example:
+
+```rust
+use base::Base;
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+struct User {
+    key: String,
+    name: String,
+    age: i32,
+}
+
+fn main() {
+    let base = Base::new(
+        String::from("DB_NAME"),
+        String::from("DB_PROJECT_KEY"),
+        String::from("DB_PROJECT_ID"),
+    );
+    let user: User = base
+        .get(String::from("krvb876h"))
+        .expect("There was an error!")
+        .json()
+        .expect("There was an error!");
+
+    println!("{:?}", user);
+}
+```
+
+This will print the following to the console:
+
+```raw
+User { key: "krvb876h", name: "Sarmad", age: 25 }
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
+pub mod base {
+    use reqwest::blocking::Client;
+    use reqwest::blocking::Response;
+    use reqwest::header::HeaderMap;
+    use reqwest::Method;
+    pub struct Base {
+        name: String,
+        project_key: String,
+        project_id: String,
+        host: String,
+    }
+    impl Base {
+        pub fn new(name: String, project_key: String, project_id: String) -> Self {
+            Base {
+                name: name,
+                project_key: project_key,
+                project_id: project_id,
+                host: String::from("https://database.deta.sh"),
+            }
+        }
+        pub fn get(&self, key: String) -> reqwest::Result<Response> {
+            self.request(
+                format!("/v1/{}/{}/items/{}", self.project_id, self.name, key),
+                Method::GET,
+            )
+        }
+        fn request(&self, path: String, method: Method) -> reqwest::Result<Response> {
+            let client = Client::new();
+            let url = format!("{}{}", self.host, path);
+            let mut headers = HeaderMap::new();
+            headers.insert("X-Api-Key", self.project_key.parse().unwrap());
+            let res = client.request(method, url).headers(headers).send();
+            res
+        }
+    }
+}
 #[cfg(test)]
 mod tests {
     #[test]
